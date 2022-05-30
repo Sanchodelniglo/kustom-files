03a621bce1aaec15d10697f80a0db95dfa79df79
Sanchodelniglo <rgrossard@gmail.com>
2022-05-30 13:46:32 +0200

    update bluetooth xmobar

--- config/.xmonad/scripts/paired-bt.sh
+++ config/.xmonad/scripts/paired-bt.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 green="#2de0a7"
+white="#ffffff"
 device_name=`bluetoothctl info | sed -n 's/.*Name://p'`
 
 if [[ $device_name != "" ]]; then
 	echo "<fc=$green>$device_name</fc>"
 else
-	echo "<fc=$green></fc>"
+	echo "<fc=$white></fc>"
 fi
